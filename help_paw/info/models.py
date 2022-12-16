fcd74556e33b7e44eddd9b0655073e6afc17aa0f
Юлия Орлова <zaikunets@yandex.ru>
2022-12-16 10:37:24 +0300

    User, FAQ, StaticInfo models

--- help_paw/info/models.py
+++ help_paw/info/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class FAQ(models.Model):
+    """Ответы на часто задаваемые вопросы"""
+
+    title = models.CharField(
+        'Заголовок вопроса',
+        help_text='Вопрос для FAQ',
+        max_length=200
+    )
+    answer = models.TextField(
+        'Ответ на вопрос',
+        help_text='Напишите четкий полезный ответ'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Ответы на частые вопросы'
+
+
+class StaticInfo(models.Model):
+    """Статическая информация на страничках"""
+
+    about_us = models.TextField(
+        'О компании',
+        help_text='Расскажите о миссии, команде'
+    )
+    portfolio = models.TextField(
+        'Портфолио',
+        help_text='Разместите портфолио команды'
+    )
+    rewards = models.TextField(
+        'Награды',
+        help_text='Разместите информацию о наградах'
+    )
+    contacts = models.TextField(
+        'Контакты',
+        help_text='Укажите контакты'
+    )
+
+    def __str__(self):
+        return self.about_us
--- help_paw/users/models.py
+++ help_paw/users/models.py
@@ -1,3 +1,70 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """ Пользователи проекта с ролями:
+        Аноним,
+        Аутентифицированный пользователь,
+        Владелец приюта,
+        Волонтер приюта,
+        Модератор контента,
+        Администратор
+    """
+
+    USER = 'user'
+    SHELTER_OWNER = 'shelre_owner'
+    VOLUNTEER = 'volunteer'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+    ROLE_CHOICES = (
+        (USER, 'Аутентифицированный пользователь'),
+        (SHELTER_OWNER, 'shelre_owner'),
+        (VOLUNTEER, 'volunteer')
+        (ADMIN, 'Администратор'),
+        (MODERATOR, 'Модератор контента')
+    )
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        help_text='Введите имя пользователя',
+        max_length=150,
+        null=False,
+        unique=True
+    )
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        help_text='Введите адрес эл. почты',
+        unique=True,
+        max_length=254
+    )
+    status = models.CharField(
+        verbose_name='Статус',
+        help_text='Выберите статус пользователя',
+        max_length=50,
+        choices=ROLE_CHOICES,
+        default=USER
+    )
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    @property
+    def is_shelter_owner(self):
+        return self.role == self.SHELTER_OWNER
+
+    @property
+    def is_volunteer(self):
+        return self.role == self.VOLUNTEER
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
