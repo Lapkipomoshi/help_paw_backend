772fdfed20451ba3a5aae7f99f49ea204eecbba4
Barinov Maksim <craftyplonkton@gmail.com>
2022-12-17 21:27:18 +0300

    Add models: Shelter, Task, Vacancy, OwnVacancy, Image

--- help_paw/info/models.py
+++ help_paw/info/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+from ..shelters.models import Shelter
+
+
+class Vacancy(models.Model):
+    shelter = models.ForeignKey(
+        Shelter, related_name='vacancy', on_delete=models.CASCADE
+    )
+    position = models.CharField(
+        'Доложность', max_length=30, help_text='Введите название должности'
+    )
+    description = models.TextField(
+        'Описание', help_text='Опишите подробности вакансии'
+    )
+    pub_date = models.DateField('Дата публикации', auto_now_add=True)
+    is_closed = models.BooleanField('Вакансия закрыта', default=False)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+        ordering = ('-pub_date', )
+
+
+class OwnVacancy(models.Model):
+    position = models.CharField(
+        'Доложность', max_length=30, help_text='Введите название должности'
+    )
+    description = models.TextField(
+        'Описание', help_text='Опишите подробности вакансии'
+    )
+    pub_date = models.DateField('Дата публикации', auto_now_add=True)
+    is_closed = models.BooleanField('Вакансия закрыта', default=False)
+
+    class Meta:
+        verbose_name = 'Своя Вакансия'
+        verbose_name_plural = 'Свои Вакансии'
+        ordering = ('-pub_date',)
+
+
+class Image(models.Model):
+    image = models.ImageField(
+        'Изображение', upload_to='temporary/',
+        help_text='Выберите изображение для загрузки'
+    )
+    pub_date = models.DateTimeField('Дата загрузки', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
+        ordering = ('-pub_date', )
--- help_paw/shelters/models.py
+++ help_paw/shelters/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+from ..info.models import Image
+
+
+class Shelter(models.Model):
+    name = models.CharField(
+        'Название', max_length=30, unique=True,
+        help_text='Введите название приюта'
+    )
+    description = models.TextField(
+        'Описание приюта', help_text='Добавьте описание приюта'
+    )
+    logo = models.ImageField(
+        'Логотип', null=True, blank=True, help_text='Загрузите логотип приюта'
+    )
+    address = models.TextField(
+        'Адрес', null=True, blank=True, help_text='Укажите адрес приюта'
+    )
+    long = models.DecimalField('Долгота', max_digits=9, decimal_places=6)
+    lat = models.DecimalField('Широта', max_digits=9, decimal_places=6)
+    gallery = models.ManyToManyField(
+        Image, verbose_name='Галлерея изображений'
+    )
+
+    class Meta:
+        verbose_name = 'Приют'
+        verbose_name_plural = 'Приюты'
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    shelter = models.ForeignKey(
+        'Shelter', related_name='task', on_delete=models.CASCADE
+    )
+    name = models.CharField(
+        'Краткое описание', max_length=50,
+        help_text='Напишите краткое описание задачи'
+    )
+    description = models.TextField(
+        'Описание задачи', help_text='Опишите подробности задачи'
+    )
+    pub_date = models.DateField('Дата публикации', auto_now_add=True)
+    is_emergency = models.BooleanField('Срочная задача', default=False)
+    is_finished = models.BooleanField('Задача завершена', default=False)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+        ordering = ('-pub_date', )
+
+    def __str__(self):
+        return self.name
