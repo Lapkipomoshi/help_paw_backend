2c5ce300c2b10c5ca1af44db712d975720a7eec2
Юлия Орлова <zaikunets@yandex.ru>
2023-01-27 17:26:46 +0300

    Изменены permissions IsAuthenticatedOrReadOnly

--- help_paw/api/views.py
+++ help_paw/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.filters import SearchFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from api.serializers import NewsSerializer, FAQSerializer
 from info.models import News, FAQ
@@ -10,14 +9,11 @@ class NewsViewSet(viewsets.ModelViewSet):
     """Новости приютов"""
     queryset = News.objects.all()
     serializer_class = NewsSerializer
-    permission_classes = (IsAuthenticated,)
-    filter_backends = (SearchFilter,)
-    search_fields = ('header',)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class FAQViewSet(viewsets.ModelViewSet):
     """Ответы на часто задаваемые вопросы"""
     queryset = FAQ.objects.all()
     serializer_class = FAQSerializer
-    permission_classes = (IsAuthenticated,)
-    filter_backends = (SearchFilter,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
