fa006b63fabf07466695a728c2fc15eb854c2241
Barinov Maksim <craftyplonkton@gmail.com>
2023-05-24 01:56:38 +0300

    news new routs

--- help_paw/api/permissions.py
+++ help_paw/api/permissions.py
@@ -42,3 +42,13 @@ class IsShelterOwnerOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.user.shelter == obj.shelter or
                 request.user.is_admin)
+
+
+class IsAuthor(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.author
+
+
+class IsShelterOwner(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_shelter_owner
--- help_paw/api/urls.py
+++ help_paw/api/urls.py
@@ -2,15 +2,18 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (AnimalTypeViewSet, CustomUserViewSet, FAQViewSet,
-                       HelpArticleViewSet, NewsViewSet, PetViewSet,
-                       ShelterViewSet, VacancyViewSet)
-from chat.views import ChatViewSet, MessageViewSet, ShelterChatViewSet
+                       HelpArticleViewSet, PetViewSet, ShelterViewSet,
+                       VacancyViewSet)
+from chat.views import ChatViewSet, MessageViewSet, MyShelterChatViewSet
+from info.views import MyShelterNewsViewSet, NewsViewSet, ShelterNewsViewSet
 
 app_name = 'api'
 
 v1_router = DefaultRouter()
 user_router = DefaultRouter()
-v1_router.register(r'news', NewsViewSet, basename="news")
+v1_router.register(r'news', NewsViewSet, basename='news')
+v1_router.register(r'shelters/(?P<shelter_id>\d+)/news', ShelterNewsViewSet, basename='shelter_news')
+v1_router.register(r'my-shelter/news', MyShelterNewsViewSet, basename='my_shelter_news')
 v1_router.register(r'faq', FAQViewSet, basename="faq")
 v1_router.register(r'shelters', ShelterViewSet, basename='shelters')
 v1_router.register(r'help-articles', HelpArticleViewSet, basename='help_articles')
@@ -18,7 +21,7 @@ v1_router.register(r'pets', PetViewSet, basename='pets')
 v1_router.register(r'vacancies', VacancyViewSet, basename='vacancies')
 v1_router.register(r'animal-types', AnimalTypeViewSet, basename='animal_types')
 v1_router.register(r'chats', ChatViewSet, basename='chats')
-v1_router.register(r'my-shelter/chats', ShelterChatViewSet, basename='shelter_chats')
+v1_router.register(r'my-shelter/chats', MyShelterChatViewSet, basename='my_shelter_chats')
 v1_router.register(r'chats/(?P<chat_id>\d+)/messages', MessageViewSet, basename='messages')
 user_router.register(r'users', CustomUserViewSet, basename='users')
 
