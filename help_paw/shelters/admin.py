0c1a6aefb61632ebd67c7bdd4abf302f6da5d359
Алексей Федотов <fedotov.alexm@gmail.com>
2023-08-07 00:33:37 +0200

    migrations fixed

--- help_paw/shelters/admin.py
+++ help_paw/shelters/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from shelters.models import AnimalType, Pet, Shelter, Task, Breed
+from shelters.models import AnimalType, Pet, Shelter, Task
 
 admin.site.register(Pet)
 admin.site.register(Shelter)
 admin.site.register(Task)
 admin.site.register(AnimalType)
-admin.site.register(Breed)
 
 admin.site.unregister(Group)
